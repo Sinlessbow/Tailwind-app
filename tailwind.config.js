f295d2d1b33915f1dabd1b322307a604d2bd2ab4
NGuinto <fenerguinto@gmail.com>
2022-09-22 14:16:03 -0500

    Se agrega el proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  content: [
+    './public/index.html',
+    './src/**/*.{html,js}',
+
+  ],
+  media: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "bookmark-purple": "#5267DF",
+        "bookmark-red": "#FA5959",
+        "bookmark-blue": "#242A45",
+        "bookmark-grey": "#9194A2",
+        "bookmark-white": "#f7f7f7",
+        "bookmark-black": "#000000",
+        "bookmark-pink": "#f535aa",
+
+      },
+    },
+    fontFamily: {
+      Poppins: ["Poppins, sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
+
